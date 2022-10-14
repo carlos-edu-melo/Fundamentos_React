523b196099916054f53c48d2efeda0d336da6313
Carlos Melo <carlos.melo@unisuam.edu.br>
2022-10-14 17:16:08 -0300

    Parametros

---
+++ src/components/parametros.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ListOfMenus from "./repeticao";
+
+export default function Parametros(props: any): JSX.Element {
+  return (
+    <>
+    <i>{props.title}</i>
+    </>
+  );
+}
